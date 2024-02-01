93a3f686568b48924341a810f66ad5a1b58e7df0
Arsen <rayymbekarsen@gmail.com>
2024-02-02 01:29:50 +0600

    комментарий

---
+++ week3/Modules.py
@@ -0,0 +1,13 @@
+#ex1
+import mymodulo
+
+#ex2
+import mymodulo as mx
+
+#ex3
+import mymodulo
+
+print(dir(mymodulo))
+
+#ex4
+from mymodulo import person1
---
+++ week3/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+import mymodulo
+
+print(dir(mymodulo))
